e3fef30727f32020c4ff96c8670d0ab0f17be90b
Matus Goljer <matus.goljer@gmail.com>
2016-03-12 19:53:59 +0100

    Support multiple input links.

--- extractor.hs
+++ extractor.hs
@@ -1,18 +1,26 @@
 import Control.Applicative
 import Control.Monad
 
+import Data.String.Utils as S
 import qualified Data.ByteString.Lazy.Char8 as C
 import Network.HTTP.Conduit
 import System.Environment
 import System.Process
 import Text.HTML.TagSoup
 
-process url = do
+getYoutubeUrls :: String -> IO [String]
+getYoutubeUrls url = do
   src <- parseTags <$> simpleHttp url
   let iframes = sections (~== "<iframe>") src
       youtubeUrls = map (C.unpack . fromAttrib (C.pack "src") . head) iframes
-  videoData <- lines <$> readProcess "youtube-dl" (["-q", "-g"] ++ youtubeUrls) ""
-  void $ spawnProcess "vlc" videoData
+  return $ map (S.replace "embed/" "watch?v=") youtubeUrls
 
 main :: IO ()
-main = getArgs >>= process . head
+main = do
+  args <- getArgs
+  youtube <- concat <$> mapM
+             (\x -> do
+                 putStrLn $ "Processing" ++ x
+                 getYoutubeUrls x)
+             args
+  void $ spawnProcess "vlc" youtube
