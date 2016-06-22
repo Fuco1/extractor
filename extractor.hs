e4c2c9ccd8a9efc255ef7b469a85aee5aee2d031
Matus Goljer <matus.goljer@gmail.com>
2016-06-23 00:05:37 +0200

    Implement printing of links only

--- extractor.hs
+++ extractor.hs
@@ -2,9 +2,11 @@ import Control.Applicative
 import Control.Monad
 
 import Data.String.Utils as S
+import Data.List (intercalate)
 import qualified Data.ByteString.Lazy.Char8 as C
 import Network.HTTP.Conduit
 import System.Environment
+import System.Console.GetOpt
 import System.Process
 import Text.HTML.TagSoup
 
@@ -15,12 +17,23 @@ getYoutubeUrls url = do
       youtubeUrls = map (C.unpack . fromAttrib (C.pack "src") . head) iframes
   return $ map (S.replace "embed/" "watch?v=") youtubeUrls
 
+data Flag = PrintOnly
+  deriving (Eq, Ord, Show)
+
+options :: [OptDescr Flag]
+options =
+  [ Option "p" ["print"] (NoArg PrintOnly) "Print the extracted links to stdout"
+  ]
+
 main :: IO ()
 main = do
   args <- getArgs
+  let (flags, links, _) = getOpt RequireOrder options args
   youtube <- concat <$> mapM
              (\x -> do
                  putStrLn $ "Processing: " ++ x
                  getYoutubeUrls x)
-             args
-  void $ spawnProcess "vlc" youtube
+             links
+  if PrintOnly `elem` flags
+    then putStrLn $ intercalate "\n" youtube
+    else void $ spawnProcess "vlc" youtube
